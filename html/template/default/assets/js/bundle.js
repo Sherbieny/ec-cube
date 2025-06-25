3ceebe88be2c56f4e48c0e5f039cfbdba234482b
sherbieny <sherbieny@outlook.com>
2025-06-25 06:09:06 +0000

    Add default scope custom js and scss Set common shared scope for js and scss

--- html/template/default/assets/js/bundle.js
+++ html/template/default/assets/js/bundle.js
@@ -1,8 +1,14 @@
-const $ = require('jquery');
+const $ = require("jquery");
 global.$ = global.jQuery = $;
 
-require('slick-carousel');
-require('slick-carousel/slick/slick.css');
-require('slick-carousel/slick/slick-theme.css');
+require("slick-carousel");
+require("slick-carousel/slick/slick.css");
+require("slick-carousel/slick/slick-theme.css");
 
-require('bootstrap');
+require("bootstrap");
+
+//Custom Shared JS
+require("shared/initializer.js");
+
+//Custom Default JS
+require("./initializer.js");
---
+++ html/template/default/assets/js/customize/test.js
@@ -0,0 +1,9 @@
+class Test {
+    constructor() {}
+
+    init() {
+        console.log("Default Test Class init");
+    }
+}
+
+module.exports = Test;
